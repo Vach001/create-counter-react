ab60763580c2e186e2971d76cf3302f4e978000d
Vache Apinyan <apinyan.v@gmail.com>
2023-06-25 11:29:53 +0400

    feat(*): add dinamic change counter

--- src/Components/Counter/Counter.jsx
+++ src/Components/Counter/Counter.jsx
@@ -1,14 +1,22 @@
-import React, { useState } from 'react';
+import React, { useState } from "react";
+import styles from "./counter.module.css";
 
 function Counter() {
-   const [counter, setCounter] = useState(0);
-   return (
-      <div className='counter-container'>
-         <button onClick={() => setCounter(counter + 1)}>Increment</button>
-         <button onClick={() => setCounter(counter - 1)}>Decrement</button>
-         <button onClick={handleReset}>Reset</button>
-         <p>Count: {counter}</p>
-      </div>
-   );
+  const [counter, setCounter] = useState(0);
+  const [initialCount, setInitialCount] = useState(0);
+  const handleInitialCountChange = (event) => {
+    setInitialCount(event.target.value);
+  };
+  const handleReset = () => {
+    setCounter(initialCount);
+  };
+  return (
+    <div className={styles.counterContainer}>
+      <button onClick={() => setCounter(counter + 1)}>Increment</button>
+      <button onClick={() => setCounter(counter - 1)}>Decrement</button>
+      <button onClick={handleReset}>Reset</button>
+      <p>Count: {counter}</p>
+    </div>
+  );
 }
 export default Counter;
